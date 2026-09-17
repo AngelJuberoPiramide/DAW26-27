e40b1f237c3b89bbd645bb43284aca45dee15459
Angel Jubero <230267@fppiramide.com>
2026-09-17 11:04:23 +0200

    upfate

---
+++ B2_01_condicional.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Ejercicio 1: ¿Mayor o menor de edad? (Condicional if)
+Concepto: Comprobar una condición con if y else.
+Instrucción: Define una variable $edad. Si es mayor o igual a 18, muestra
+"Puedes pasar", si no, muestra "Acceso denegado".
+*/
+
+    $edad = "18";
+    if ($edad >= 18){
+        echo "Puedes pasar.";
+    } else {
+        echo "Acceso denegado.";
+    }
+?>
---
+++ B2_02_bucles.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Ejercicio 2: Contar del 1 al 5 (Bucle for)
+Concepto: Repetir una instrucción varias veces con un bucle.
+Instrucción: Muestra los números del 1 al 5, cada uno en una línea distinta
+(usa <br> para saltar de línea en HTML).
+*/
+
+    for($i = 1; $i <= 5; $i++) {
+        echo $i . "<br>";
+    }
+?>
