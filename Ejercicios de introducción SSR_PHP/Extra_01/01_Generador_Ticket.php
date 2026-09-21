4c83a5f0e5b16b976a6b06d9c910baf481341f4d
AngelJuberoPiramide <230267@fppiramide.com>
2026-09-21 14:18:49 +0200

    update

---
+++ Ejercicios de introducción SSR_PHP/Extra_01/01_Generador_Ticket.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <form>
+        <label>Nombre del producto: </label>
+        <label>Precio unitario: </label>
+        
+    </form>
+
+<?php
+
+/*
+Enunciado 1: Generador de Ticket de Compra Dinámico
+Objetivo
+Desarrollar una aplicación web en un solo archivo PHP autoprocesado que calcule e imprima el ticket de compra de un producto, aplicando impuestos y descuentos dinámicamente según el perfil del cliente.
+*/
+
+
+
+?>
+</body>
+</html>
