2f8d63474fb081d28f4027ca06e2b40169c63202
AngelJuberoPiramide <230267@fppiramide.com>
2026-09-21 13:39:47 +0200

    update

--- B3_03_funcionEmpty.php
+++ B3_03_funcionEmpty.php
@@ -6,6 +6,12 @@ Concepto: La función empty() comprueba si una variable no tiene ningún valor e
 Instrucción: Verifica si el usuario envió un campo vacío desde un formulario.
 */
 
+$variable = 1;
 
+if(empty($variable)) {
+    echo "La variable esta vacia.";
+} else {
+    echo "La variable no esta vacia.";
+}
 
 ?>
