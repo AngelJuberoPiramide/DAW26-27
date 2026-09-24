e43f452eff05d95a7ccafe5c2fc3fe05d7429437
AngelJuberoPiramide <230267@fppiramide.com>
2026-09-24 10:16:11 +0200

    update

---
+++ Entorno_Cliente(Pablo)/Ejercicios_practica_UD_2/Ejercicios_practica_UD_2.js
@@ -0,0 +1,3 @@
+/*Declara 6 variables a las que asignaremos los siguientes valores. 1357, 135.7, 135e7, 0b1010, 0o1357 y 0x1A57.
+Una vez creadas muestra por consola los valores almacenados y el tipo de dato que nos indica el operador typeof.*/
+
